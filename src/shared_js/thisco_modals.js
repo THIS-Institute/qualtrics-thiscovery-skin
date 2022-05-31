43dd9a8bd094b2275776ea36f96e5130a9a03b8a
Glyn Cannon <glynternet@mac.com>
2022-05-31 18:06:35 +0100

    bleurgh

--- src/shared_js/thisco_modals.js
+++ src/shared_js/thisco_modals.js
@@ -5,13 +5,13 @@ import A11yDialog from 'a11y-dialog';
 module.exports = function(){
     const dialogs = Bliss.$("dialog").forEach(el=>{
         el.close(); // close all dialogs on setup
-        const open_link = Bliss.$(`a[href="modal:${el.id}"]`)[0];
+        const open_link = Bliss.$(`a[href="modal?${el.id}"]`)[0];
         if (!open_link) {
             console.warn("Dialog element included but has no anchor connected to open it");
             return;
         }
 
-        // replace dialog with a11y-dialog markup
+        // replace dialog with a11y-dialog markup 
         const modal = Bliss.create("div",{
             id : el.id,
             className : "dialog-container",
