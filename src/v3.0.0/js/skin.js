638d149b756bc82702f6d522fd33fb1e77ee4ae1
Glyn Cannon <glynternet@mac.com>
2022-07-13 11:12:00 +0100

    3.0.0 consent foms

--- src/v3.0.0/js/skin.js
+++ src/v3.0.0/js/skin.js
@@ -4,10 +4,13 @@
 // 3.0.0 is a forward-looking version for non-qualtrics surveys
 // and standardised markup
 
+const version = "3.0.0";
+console.log(`Thiscovery survey skin version ${version}`);
+
+const revision = 2;
+console.log(`Revision: ${revision}`);
 
 const BlissfulJs = require('blissfuljs'); // module adds Bliss to window object for us
-const version = require('../../../package.json').version;
-console.debug(`skin.js version ${version}`);
 import { forIn, startCase } from 'lodash';
 
 const BACK_LINK = window.location.search.includes("staging") ? "https://staging.thiscovery.org/my-tasks/" : "https://www.thiscovery.org/my-tasks/";
@@ -89,4 +92,35 @@ if (likertScale.length) {
 
 console.debug('ranking questions');
 require("../../shared_js/ranking_question.js")();
+
+// non-Qualtrics consent statements [will one day require own webhook behaviour?]
+
+const isConsentForm = $$(".consent-checklist").length > 0;
+if (isConsentForm) {
+
+    console.debug('Has consent form');
+
+    $$(".thisco-survey .consent-checklist").forEach(checklist=>{
+        let fset = checklist.closest("fieldset");
+        checklist.className.split(" ").forEach(cl=>{
+            fset.classList.add(cl);
+        });
+        if (fset.classList.contains('consent-switches')) {
+            Bliss.$("label",fset).forEach(label=>{
+                label.classList.add("thisco-response-label");
+                label.appendChild(Bliss.create("button",{
+                    className : "consent-switch",
+                    contents : [
+                        {tag:"span",contents:"Yes"},
+                        {tag:"span",contents:"No"}
+                    ]
+                }))
+            });
+        }
+        checklist.className = "";
+    });
+
+}
+
+
 
