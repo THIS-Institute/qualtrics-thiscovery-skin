c87b82f36818f778ec0e277da85bf09e135c67d3
Glyn Cannon <glynternet@mac.com>
2023-02-02 09:38:01 +0000

    Removed /out from repo and added a doc build step when running read_docs

--- src/shared_js/explainer.js
+++ src/shared_js/explainer.js
@@ -1,6 +1,14 @@
 const BlissfulJs = require('blissfuljs'); // module adds Bliss to window object for us, use Bliss. and Bliss.$. for $ and $$
 // uses disposableModal method from thisco_modals.js
 
+/**
+ * 
+ * For any element with `[data-explainer]` adds a '?' button which
+ * fires disposableModal with attribute's value as the body HTML
+ * 
+ * @module
+ */
+
 module.exports = function(){
     Bliss.$("[data-explainer]").forEach(el=>{
         const explain = el.dataset.explainer || "";
