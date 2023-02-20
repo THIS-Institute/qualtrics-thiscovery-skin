9eaa0996d17afc63470fb6847218742e6e4d0b36
Glyn Cannon <glynternet@mac.com>
2023-02-20 14:06:56 +0000

    pushing to 2.1 and 2.2 so can test illustrations in Q

--- src/shared_js/illustrations.js
+++ src/shared_js/illustrations.js
@@ -3,7 +3,7 @@ const debug = require('debug')('thisco:illustrations.js');
 
 module.exports = function(){
 
-    load("https://thiscovery-public-assets.s3.eu-west-1.amazonaws.com/shared_modules/thisco-illustrations.js",(err)=>{
+    load("https://thiscovery-public-assets.s3.eu-west-1.amazonaws.com/shared_modules/thisco-illustrations.js",{"type":"module"},(err)=>{
         if (err) {
             throw("Error fetching thisco-illustrations component",err);
         }
